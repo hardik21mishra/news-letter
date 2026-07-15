6b9cc5e2ab9cc55b9a27d0872db99329ee685297
Hardik <hardik.mishra2022@gmail.com>
2026-07-15 16:09:56 +0530

    did some changes undo

--- socialmedia.py
+++ socialmedia.py
@@ -6,7 +6,3 @@ chat_id = "1148674997"
 message = the_news()
 linkkk = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
 print(requests.get(linkkk).json())
-
-
-# okayy so whatever change I do in here, they must not reflect back 
-# in the master branch since this is a send_message branch
