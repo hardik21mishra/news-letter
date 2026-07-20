51bbe0bf171a9629b59c1cead1a388f051aa4e35
Hardik <hardik.mishra2022@gmail.com>
2026-07-20 11:32:17 +0530

    add json file

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from fetch_news import fetch_news
 from categorize import process_news
+import json 
+
+def save_to_json(articles):
+    with open("news.json", "w") as f:
+        json.dump(articles, f, indent = 2)
+    print("saved articles to news.json")
 
 def the_news():
     articles = fetch_news()
     articles = articles[:10]
     processed_articles = process_news(articles)
+    save_to_json(processed_articles)
     return processed_articles
 
 if __name__ == "__main__":
