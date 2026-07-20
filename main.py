69249ab525fb5f28d04591b7047fa40c743ba56d
Hardik <hardik.mishra2022@gmail.com>
2026-07-20 18:21:58 +0530

    update

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ from fastapi import FastAPI
 from app import the_news
  
 app = FastAPI()
- 
- 
+
 @app.get("/")
 def home():
     return {"message": "Hello, this is my news API!"}
- 
- 
+  
 @app.get("/news")
 def get_news(category: str = None):
     articles = the_news()
