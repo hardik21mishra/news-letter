028d2efcab288112c2106eb3f015be8d5c3f1823
Hardik <hardik.mishra2022@gmail.com>
2026-07-20 13:22:06 +0530

    add fastapi with categroy filter

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from app import the_news
+ 
+app = FastAPI()
+ 
+ 
+@app.get("/")
+def home():
+    return {"message": "Hello, this is my news API!"}
+ 
+ 
+@app.get("/news")
+def get_news(category: str = None):
+    articles = the_news()
+ 
+    if category:
+        articles = [a for a in articles if a["category"] == category]
+ 
+    return articles
+ 
