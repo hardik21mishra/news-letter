7997c202fee723bfc5ef17b9d8321b0cc2e0d5f1
Hardik <hardik.mishra2022@gmail.com>
2026-08-19 13:05:30 +0530

    testing...

--- fetch_news.py
+++ fetch_news.py
@@ -125,7 +125,7 @@ def fetch_news():
     articles = []
     for feed_url, source in feed_links.items():
         feed = feedparser.parse(feed_url)
-        for entry in feed.entries[:3]:
+        for entry in feed.entries[:1]:
             description = (
                 entry.get("summary")
                 or entry.get("description")
