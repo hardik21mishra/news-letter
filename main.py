e9f2abdaff8db457ddf7aa5b8f58ad83c4ae5dfe
Hardik <hardik.mishra2022@gmail.com>
2026-09-08 09:31:56 +0530

    origin link fix

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "http://localhost:5178",
+        "http://127.0.0.1:5178",
     ],
     allow_credentials=True,
     allow_methods=["*"],
